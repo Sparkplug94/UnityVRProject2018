457e9ae5778967038b560ff8087399ff8dd498f3
dblack12705 <dblack12705@gmail.com>
2018-05-30 15:25:05 -0700

    boxes now display their index and have flags and poles hidden. NOTE: way to make text look less shitty is to scale down the text size and scale up the font size

--- Assets/mainScript.js
+++ Assets/mainScript.js
@@ -10,35 +10,55 @@ var process : System.Diagnostics.Process;
 
 
 
-var objects = new List.<GameObject>();
+var cubes = new List.<GameObject>();
 var rows : int;
 var cols : int;
 var mineFreq : float;
-var numberOfCubes : int = 3;
+var numberOfCubes : int;
 
 //this is a C# style "array", use "Count" instead of "Length" and 
 //you can't access an array element until you've created it 
 
 
 function Start(){
-	//add flagcubes to objects array
-	makeCubes();
-	//get children of things in objects array
-	var flagtextobject = objects[0].transform.GetChild(0);
-	//get the meshrenderer component of the first child of objects[0]
-	var mr = flagtextobject.GetComponent(MeshRenderer);
+
+	//set the parameters
+	rows = 8;
+	cols = 8;
+	numberOfCubes = rows*cols;
+	mineFreq = 0.5;
+
+	//make the cubes
+	for (var ii = 0; ii < rows; ii++){
+		for (var jj = 0; jj < cols; jj++){
+			cubes.Add(Instantiate(Resources.Load("FlagCube")));
+			cubes[ii*rows+jj].transform.position = Vector3(2*ii,0,2*jj);
+			var textObject = cubes[ii*rows+jj].transform.GetChild(0);
+			var poleObject = cubes[ii*rows+jj].transform.GetChild(1);
+			var flagObject = poleObject.transform.GetChild(0);
+			//set texts to index
+			textObject.GetComponent(TextMesh).text = ii.ToString()+","+jj.ToString();
+			flagObject.GetComponent(MeshRenderer).enabled = false;
+			poleObject.GetComponent(MeshRenderer).enabled = false;
+		}
+	}
+
+	//get children of things in cubes array
+	//var flagtextobject = cubes[0].transform.GetChild(0);
+	//get the meshrenderer component of the first child of cubes[0]
+	//var mr = flagtextobject.GetComponent(MeshRenderer);
 	//enable or disable the renderer (hide and show text)
-	mr.enabled = true;
-	//get the textmesh component of first child of objects[0] which happens to be the "Text" object
-	var flagtext = flagtextobject.GetComponent(TextMesh);
+	//mr.enabled = true;
+	//get the textmesh component of first child of cubes[0] which happens to be the "Text" object
+	//var flagtext = flagtextobject.GetComponent(TextMesh);
 	//change the text property of TextMesh to "byebye"
-	flagtext.text = "byebye";
+	//flagtext.text = "byebye";
+	
+
+
 
 
 	//make the mines - make this into a function
-	rows = 8;
-	cols = 8;
-	mineFreq = 0.5;
 	var mineGrid = new int[rows, cols];
 	for (var i = 0; i < rows; i++) {
 		for (var j = 0; j < cols; j++){
@@ -113,7 +133,10 @@ function Start(){
 	}
 
 
-//now we have mine grids and a number grid
+	//now we have mine grids and a number grid
+
+
+	
 
 
 }
@@ -123,11 +146,7 @@ function Update(){
 	
 }
 
-function makeCubes(){
-	for (var i = 0; i < numberOfCubes; i++){
-		objects.Add(Instantiate(Resources.Load("FlagCube")));
-	}
-}
+
 
 
 
